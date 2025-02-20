ca1bab92c9027d2c3f0b7df048cf2de4f7e8aadf
ZNishad <zulfuqarlinishat@gmail.com>
2025-02-20 16:44:54 +0400

    as

--- MovieApp/WatchList/WatchListController.swift
+++ MovieApp/WatchList/WatchListController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class WatchListController: UIViewController {
+
     init() {
         super.init(nibName: nil, bundle: nil)
         tabBarItem = .init(
@@ -23,5 +24,34 @@ class WatchListController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .pageBack
+        view.addSubviews(emptyImage, watchListLabel)
+        setupUI()
     }
+
+    private lazy var watchListLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Watch List"
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+
+    private let emptyImage: UIImageView = {
+        let image = UIImage(resource: .emptyWatchList)
+        let imageView = UIImageView(image: image)
+        return imageView
+    }()
+
+    private func setupUI() {
+        watchListLabel.top(view.safeAreaLayoutGuide.topAnchor, -24).0
+            .leading(view.leadingAnchor, 24).0
+            .trailing(view.trailingAnchor, -24).0
+            .height(27)
+
+        emptyImage.centerX(view.centerXAnchor).0
+            .centerY(view.centerYAnchor).0
+            .width(252).0
+            .height(190)
+    }
+
 }
